dce8d6d615501e046f23f546eeaf8391d43884fc
Jango-070 <jango.room@hotmail.com>
2020-12-23 23:02:04 +0100

    Gebeleven bij het maken van de Even.target.value, maken imagelinkform wordt nog gedaan met this.onInputChange

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = ({onInputChange}) => {
+	return(
+		<div>
+		<p classsName ='f3'>
+			{`Met deze app kan je een gezicht detecteren van je foto.`}
+		</p>
+		<div className='center'>
+			<div className='form center pa4 br3 shadow-5'>
+				<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+				<button 
+				className='w-60 grow f4 link ph3 pv2 dib white bg-light-purple'> 
+				detecteer gezicht
+				</button>
+			</div>
+		</div>
+		</div>
+	);
+}
+
+export default ImageLinkForm;
